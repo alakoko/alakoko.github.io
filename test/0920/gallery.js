8616392cbed0847366ba4f996da9aee74bd41e14
alakoko <alakoko.kim@gmail.com>
2015-09-20 11:43:58 +0900

    오타 수정
    오타 수정

--- test/0920/gallery.js
+++ test/0920/gallery.js
@@ -21,7 +21,7 @@
 		};
 
 		$.each(data, function  (i, item) {
-			var sizeType = Math.ceil(Math.randeom()*2);
+			var sizeType = Math.ceil(Math.random()*2);
 			var itemHTML =	'<li class="size-'+sizeType+'">' +
 									'<a href="http://power4ce.cafe.com/images/'+item.images.large+' ">' +
 										'<img class="img-loading" src="http://power4ce.cafe24.com/images/' +
