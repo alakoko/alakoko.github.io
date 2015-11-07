557ece32a6e6788775917b9255ba7cbd4dbae0fb
alakoko <alakoko.kim@gmail.com>
2015-11-08 02:38:31 +0900

    portfolio 페이지 tab 메뉴 추가
    portfolio 페이지 tab 메뉴 추가 및 skip nav IR

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,61 @@
-$(document).ready(function() {
-	$('#project').hide();
+(function($){
+	$('#project, #example, #career').hide();
 	$('.tab li').click(function  () {
 		var activeTab = $(this).attr("rel");
-	       	// console.log("activeTab:"+activeTab);
-
-	    var offTab = $(this).siblings('li').attr("rel");
-	    	// console.log("offTab:"+offTab);
+		console.log("activeTab:"+activeTab);
 
 		$(this).siblings().removeClass('on');
 		$(this).addClass('on');
 		$("#" + activeTab).fadeIn();
-		$("#" + offTab).hide();
 
+		$("section").not($("#" + activeTab)).hide();
 	});
-});
+
+	var report_list = document.getElementById('report_list');
+	var $report = $(report_list);
+
+		console.log("report_list" + report_list);
+		console.log("report" + $(report_list));
+
+		// 마우스 이동방향을 얻어오는 함수.
+	// 0:top, 1:right, 2:bottom, 3:left
+	var getMouseDirection = function(e){
+		var $el = $(e.currentTarget),
+			offset = $el.offset(),
+			w = $el.outerWidth(),
+			h = $el.outerHeight(),
+			x = (e.pageX - offset.left - w/2) * ( w>h ? h/w : 1),
+			y = (e.pageY - offset.top - h/2) * ( h>w ? w/h : 1),
+			direction = Math.round(((Math.atan2(y, x) * (180 / Math.PI) + 180 ) / 90 ) + 3 ) % 4;
+		return direction;
+	},
+		// 마우스오버시 나타나는 박스의 이동방향을 결정하는 함수
+	hoverDirection = function(e){
+		var $overlay = $(this).find('span'),
+			side = getMouseDirection(e),
+			animateTo,
+			positionIn = { top:0, left:0 },
+			positionOut = (function(){
+				switch(side){
+					// case0: top, case1: right, case2: bottom, default: left
+					case 0  : return {top: '-100%', left: 0      }; break;
+					case 1  : return {top: 0,       left: '100%' }; break;
+					case 2  : return {top: '100%',  left: 0      }; break;
+					default : return {top: 0,       left: '-100%'}; break;
+				}
+			})();
+		if(e.type === 'mouseenter'){
+			animateTo = positionIn;
+			$overlay.css(positionOut);
+		} else {
+			animateTo = positionOut;
+		}
+		$overlay.stop(true).animate(animateTo, 250);
+	};
+
+	//실행
+	$(report_list).children('li').addClass('report_item');
+	$(report_list).find('span').addClass('caption');
+	$report.on('mouseenter mouseleave', '.report_item a', hoverDirection);
+
+})(jQuery);
