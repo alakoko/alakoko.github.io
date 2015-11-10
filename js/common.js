f6e45ef0f709d18b8cfb04593f6752a72f94669b
alakoko <alakoko.kim@gmail.com>
2015-11-10 23:35:29 +0900

    불필요한 CSS파일 정리

--- js/common.js
+++ js/common.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+	var $btnTop = $('#back_top');
 	if (window.location.href && document.getElementById('CP')) {
 		$('#CP').addClass('current');
 		 }
+
+	var btnTop = function(){
+			var scrt = $('body').scrollTop(),
+				wh = $(window).height() / 2;
+			if(scrt > wh) $btnTop.fadeIn();
+			else $btnTop.fadeOut();
+		}
+		btnTop();
+		$(window).on('scroll', function(){ btnTop(); });
 });
